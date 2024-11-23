8d991271d04b21a2bf6db4c00ba948c0df3fb435
Ahmed Elimam <43957299+AhmedElimam@users.noreply.github.com>
2024-11-24 00:34:59 +0200

    hotfix

--- app/Http/Controllers/FireController.php
+++ app/Http/Controllers/FireController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\Auth;
+
 class FireController extends Controller
 {
-
     protected $auth;
 
     public function __construct()
@@ -27,39 +26,53 @@ class FireController extends Controller
         ]);
 
         try {
-            $signInResult = $this->auth->signInWithEmailAndPassword(
-                $request->email,
-                $request->password
-            );
+            $signInResult = $this->auth->signInWithEmailAndPassword($request->email, $request->password);
 
             return response()->json([
                 'token' => $signInResult->idToken(),
                 'message' => 'Login successful',
             ], 200);
-        } catch (\Exception $e) {
+        } catch (\Kreait\Firebase\Exception\Auth\AuthError $e) {
             return response()->json(['error' => $e->getMessage()], 401);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json(['error' => 'An unexpected error occurred.'], 500);
         }
     }
 
     public function register(Request $request)
     {
         $request->validate([
-            'email' => 'required|email|unique:users',
+            'email' => 'required|email',
             'password' => 'required|min:6',
+            'phone_number' => ['required', 'regex:/^\+\d{1,15}$/'],
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
         ]);
 
         try {
-            $user = $this->auth->createUserWithEmailAndPassword(
-                $request->email,
-                $request->password
-            );
+            $user = $this->auth->createUser([
+                'email' => $request->email,
+                'password' => $request->password,
+                'phoneNumber' => $request->phone_number,
+            ]);
+
+            $this->auth->updateUser($user->uid, [
+                'displayName' => $request->first_name . ' ' . $request->last_name,
+            ]);
+
+            $signInResult = $this->auth->signInWithEmailAndPassword($request->email, $request->password);
 
             return response()->json([
                 'user' => $user,
+                'token' => $signInResult->idToken(),
                 'message' => 'Registration successful',
-            ], 201);
-        } catch (\Exception $e) {
+            ], 200);
+        } catch (\Kreait\Firebase\Exception\Auth\AuthError $e) {
             return response()->json(['error' => $e->getMessage()], 400);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json(['error' => 'An unexpected error occurred.'], 500);
         }
     }
 }
